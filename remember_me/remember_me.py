d8a3b095615801f91aed1f40bb0401104dc96aa0
Austin Tracy <austin.tracy@jadeglobal.com>
2024-08-14 14:44:10 -0700

    another useful tidbit

--- remember_me/remember_me.py
+++ remember_me/remember_me.py
@@ -4,3 +4,13 @@
 
 import pathlib  
 pathlib.Path.unlink(f"processes\whatever.json") 
+
+
+import subprocess
+
+# Activate the conda environment and list the environments
+command = 'C:\\Users\\User\\AppData\\Local\\anaconda3\\Scripts\\activate.bat && conda env list'
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+# Print the output
+print(result.stdout)
